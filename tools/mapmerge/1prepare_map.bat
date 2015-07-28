3035e012b0083a9cf35fa059b5a519e31a83307a
Mal <vinylspiders@gmail.com>
2015-07-28 00:00:58 -0400

    Mapmerge fix
    -fixed the mapmerge tool so that it works with our maps

--- tools/mapmerge/1prepare_map.bat
+++ tools/mapmerge/1prepare_map.bat
@@ -2,7 +2,9 @@ SET z_levels=6
 cd ../../maps
 
 FOR /L %%i IN (1,1,%z_levels%) DO (
-  copy exodus-%%i.dmm exodus-%%i.dmm.backup
+  copy Sol-England_2.dmm Sol-England_2.dmm.backup
+  copy TauCeti-Biezel.dmm TauCeti-Biezel.dmm.backup
+  copy centi2.dmm centi2.dmm.backup
 )
 
 pause
--- tools/mapmerge/2clean_map.bat
+++ tools/mapmerge/2clean_map.bat
@@ -2,7 +2,9 @@ SET z_levels=6
 cd 
 
 FOR /L %%i IN (1,1,%z_levels%) DO (
-  java -jar MapPatcher.jar -clean ../../maps/exodus-%%i.dmm.backup ../../maps/exodus-%%i.dmm ../../maps/exodus-%%i.dmm
+  java -jar MapPatcher.jar -clean ../../maps/Sol-England_2.dmm.backup ../../maps/Sol-England_2.dmm ../../maps/Sol-England_2.dmm
+  java -jar MapPatcher.jar -clean ../../maps/TauCeti-Biezel.dmm.backup ../../maps/TauCeti-Biezel.dmm ../../maps/TauCeti-Biezel.dmm
+  java -jar MapPatcher.jar -clean ../../maps/centi2.dmm.backup ../../maps/centi2.dmm ../../maps/centi2.dmm
 )
 
 pause
